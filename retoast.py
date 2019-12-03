722d21d3e4c53fcc592f59c4f23a3c3fe3514002
ZJAllen <zach.allen92@gmail.com>
2019-12-02 20:05:34 -0600

    tidied up code

--- retoast.py
+++ retoast.py
@@ -5,7 +5,6 @@ import digitalio
 import adafruit_max31855
 from temp_profile import SMD291AX as temp
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 
 
 class CurrentTemp():
@@ -28,13 +27,8 @@ line1, = ax.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
 plt.style.use('fivethirtyeight')
 plt.xlim([0, 250])
 plt.ylim([0, 250])
-
-#plt.plot(temp.x, temp.y, label='Target Temperature')
-#plt.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
 plt.legend(loc=(1.04, 0.5))
-
 plt.tight_layout()
-#plt.show()
 
 startTime = time.time()
 
@@ -53,30 +47,3 @@ for i in range(0, temp.x[-1], 1):
     fig.canvas.flush_events()
 
     time.sleep(1)
-
-
-def animate(i):
-    elapsedTime = int(time.time() - startTime)
-    currentTemp.x.append(elapsedTime)
-
-    tempC = max31855.temperature
-    currentTemp.y.append(tempC)
-
-    plt.cla()
-    plt.tight_layout()
-    plt.plot(temp.x, temp.y, label='Target Temperature')
-    plt.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
-
-
-#ani = FuncAnimation(plt.gcf(), animate, interval=1000)
-
-
-
-
-'''
-while True:
-    tempC = max31855.temperature
-    elapsedTime = int(time.time() - startTime())
-    print(f'Temperature: {tempC} C')
-    time.sleep(2.0)
-'''
