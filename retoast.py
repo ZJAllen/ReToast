5e4cb468585c582e7131914e52f63cfcde193256
ZJAllen <zach.allen92@gmail.com>
2019-12-02 19:19:24 -0600

    fixed syntax, changed order of plt calls

--- retoast.py
+++ retoast.py
@@ -25,26 +25,26 @@ plt.ylim([0, 250])
 
 plt.plot(temp.x, temp.y, label='Target Temperature')
 
-startTime = time.time()
+plt.legend(loc=(1.04, 0.5))
 
+plt.tight_layout()
+plt.show()
 
 def animate(i):
-    elapsedTime = float(int(time.time() - startTime)
+    elapsedTime = int(time.time() - startTime)
     currentTemp.x.append(elapsedTime)
 
     tempC = max31855.temperature
     currentTemp.y.append(tempC)
-    
+
     plt.cla()
+    plt.tight_layout()
     plt.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
 
 
 ani = FuncAnimation(plt.gcf(), animate, interval=1000)
 
-plt.legend(loc=(1.04, 0.5))
 
-plt.tight_layout()
-plt.show()
 
 
 '''
