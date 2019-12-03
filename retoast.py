394b6910115fdd759992dbd323176a199d425e35
ZJAllen <zach.allen92@gmail.com>
2019-12-02 19:39:54 -0600

    update plotting scheme

--- retoast.py
+++ retoast.py
@@ -19,12 +19,14 @@ spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
 cs = digitalio.DigitalInOut(board.D5)
 max31855 = adafruit_max31855.MAX31855(spi, cs)
 
+fig = plt.figure()
+
 plt.style.use('fivethirtyeight')
 plt.xlim([0, 250])
 plt.ylim([0, 250])
 
 plt.plot(temp.x, temp.y, label='Target Temperature')
-
+plt.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
 plt.legend(loc=(1.04, 0.5))
 
 plt.tight_layout()
@@ -39,10 +41,8 @@ for i in range(0, temp.x[-1], 1):
     tempC = max31855.temperature
     currentTemp.y.append(tempC)
 
-    plt.cla()
-    plt.tight_layout()
-    plt.plot(temp.x, temp.y, label='Target Temperature')
-    plt.plot(currentTemp.x, currentTemp.y, label='Current Temperature')
+    fig.canvas.draw()
+    fig.canvas.flush_events()
 
     time.sleep(1)
 
